7717f703485cc3b6b8c849f814ed2e0d40847d6c
jmin07 <jmbaek07@gmail.com>
2024-08-01 09:21:31 +0900

    백종민: 1주차

--- 01-Mathematics/Jongmin/B11653/B11653.java
+++ 01-Mathematics/Jongmin/B11653/B11653.java
@@ -1,7 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-public class B2747 {
+
+public class B11653 {
     public static void main(String[] args) {
 
     }
--- 01-Mathematics/Jongmin/B15649/B15649.java
+++ 01-Mathematics/Jongmin/B15649/B15649.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class B2747 {
+public class B15649 {
     public static void main(String[] args) {
 
     }
